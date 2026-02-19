9eb4853a570a2d881192c93b90b271fa228ed7ae
matheusado1 <matheus-alves@outlook.com.br>
2026-02-19 17:07:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+print("Jogo da Adivinhação")
 
+numero_secreto = 7  
+tentativas = 0
+
+while True:
+    palpite = int(input("Digite seu palpite: "))
+    tentativas += 1
+
+    if palpite < numero_secreto:
+        print("Muito baixo!")
+    elif palpite > numero_secreto:
+        print("Muito alto!")
+    else:
+        print(f"Acertou em {tentativas} tentativas!")
+        break
