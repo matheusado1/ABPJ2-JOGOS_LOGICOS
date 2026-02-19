8245fc298efa4a1be30764a25bc52f678f5191a2
matheusado1 <matheus-alves@outlook.com.br>
2026-02-19 20:15:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+import random
+
 print("Jogo da Adivinhação")
 
-numero_secreto = 7  
+numero_secreto = random.randint(1, 100)
+
 tentativas = 0
 
 while True:
-    palpite = int(input("Digite seu palpite: "))
-    tentativas += 1
+    try:
+        palpite = int(input("Digite um número entre 1 e 100: "))
+        
+        if palpite < 1 or palpite > 100:
+            print(" Digite um número válido entre 1 e 100!")
+            continue
 
-    if palpite < numero_secreto:
-        print("Muito baixo!")
-    elif palpite > numero_secreto:
-        print("Muito alto!")
-    else:
-        print(f"Acertou em {tentativas} tentativas!")
-        break
+        tentativas += 1
+
+        if palpite < numero_secreto:
+            print("Muito baixo!")
+        elif palpite > numero_secreto:
+            print("Muito alto!")
+        else:
+            print(f" Acertou em {tentativas} tentativas!")
+            break
+
+    except ValueError:
+        print("Digite apenas números inteiros!")
